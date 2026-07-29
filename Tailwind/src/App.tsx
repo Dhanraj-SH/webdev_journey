ec2d2705da2484e0ff49f9a9e1691937e449e5e7
Dhanraj-SH <dhanrajsh445@gmail.com>
2026-07-29 15:37:14 +0530

    Learning react native

---
+++ Tailwind/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='grid grid-cols-15'>
+        <div className='col-span-15 sm:col-span-5 bg-red-300'>hi</div>
+        <div className='col-span-15 sm:col-span-5 bg-blue-300'>hi</div>
+        <div className='col-span-15 sm:col-span-5 bg-green-300'>hi</div>
+      </div>
+    </>
+  )
+}
+
+export default App
